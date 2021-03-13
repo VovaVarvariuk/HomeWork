35681e091e5dedcc33f2f45cb25f81bf70f7d514
VovaVarvariuk <vovavarvariuk@gmail.com>
2021-03-13 10:57:10 +0200

    rename ДЗ10->ДЗ11 add ДЗ12

---
+++ ДЗ12/main.js
@@ -0,0 +1,24 @@
+let a = true && false; // false
+console.log(a);
+let a1 = true || false; // true
+console.log(a1);
+let a2 = true && !false; // true
+console.log(a2);
+let a3 = true && 5; // 5
+console.log(a3);
+let a4 = 5 && false; // false
+console.log(a4);
+let a5 = true || 5; // true
+console.log(a5);
+let a6 = !5; // false
+console.log(a6);
+let a7 = 5 && 7; // 7
+console.log(a7);
+let a8 = 5 || 2; // 5
+console.log(a8);
+let a9 = 5 && 'hi'; // 'hi'
+console.log(a9);
+let a10 = 5 || 'hi'; // 5
+console.log(a10);
+let a11 = !5 && 7; // false
+console.log(a11);
