9a83b51ed2c207505afe2a7aa50939cce3b9b86b
VovaVarvariuk <vovavarvariuk@gmail.com>
2021-03-13 11:24:58 +0200

    update ДЗ12

--- ДЗ12/main.js
+++ ДЗ12/main.js
@@ -22,3 +22,12 @@ let a10 = 5 || 'hi'; // 5
 console.log(a10);
 let a11 = !5 && 7; // false
 console.log(a11);
+
+let b = true && 7 || false && '';  //true && 7 = 7; 7 || false = 7; 7 && '' = 7;
+console.log(b);
+
+let b1 = 0 || 3 && true && !3; // 0 || 3 = 3; 3 && true = true; true && !3 = false;
+console.log (b1);
+
+let b2 = !!!false && !5 || 1 && true; // !!!false && !5 = false; !5 || 1 = 1; 1 && true = true;
+console.log (b2);
